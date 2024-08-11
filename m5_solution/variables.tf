43e09f7485687cb6c636b5718da2b6ffdfe61005
Shahar Dambo <sdambo@gmail.com>
2024-08-11 14:48:01 +0300

    done

--- m5_solution/variables.tf
+++ m5_solution/variables.tf
@@ -1,19 +1,7 @@
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-  sensitive   = true
-}
-
 variable "aws_region" {
   type        = string
   description = "Region for AWS Resources"
-  default     = "us-east-1"
+  default     = "eu-central-1"
 }
 
 variable "enable_dns_hostnames" {
